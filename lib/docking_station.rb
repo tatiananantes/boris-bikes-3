b1266e88e48fa091eef134784dd059593513894a
Mark McLeod <markmcleod45@gmail.com>
2021-10-21 16:14:07 +0100

    Added 'returns docked bikes' test, updated 'docks a bike' test, and made them pass

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,14 @@
 require_relative '../lib/bike.rb'
 class DockingStation
-    def release_bike
-        Bike.new
-    end
+  
+  attr_reader :bike
+
+  def release_bike
+    Bike.new
+  end
+
+  def dock(bike)
+    @bike = bike
+  end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,11 +5,30 @@ describe DockingStation do
     #   docking_station = DockingStation.new
     #   expect(docking_station).to respond_to{docking_station.release_bike}
     # end
-    it { is_expected.to respond_to{docking_station.release_bike} }
+  it { is_expected.to respond_to{docking_station.release_bike} }
 
-    it "creates a new bike when the release bike method is called" do
-        station = DockingStation.new
-        expect{bike = station.release_bike}.not_to raise_error 
-    end
+  it "creates a new bike when the release bike method is called" do
+    station = DockingStation.new
+    expect{bike = station.release_bike}.not_to raise_error 
+  end
+
+  it "docks a bike" do
+    docking_station = DockingStation.new
+    bike = Bike.new
+    expect(docking_station).to respond_to(:dock).with(1).argument
+    expect(docking_station.dock(bike)).to eq bike
+  end
+
+  it "checks if docking station has a bike" do
+    docking_station = DockingStation.new
+    expect(docking_station).to respond_to(:bike)
+  end
+
+  it 'returns docked bikes' do
+    docking_station = DockingStation.new
+    bike = Bike.new
+    docking_station.dock(bike)
+    expect(docking_station.bike).to eq bike
+  end
 
 end
